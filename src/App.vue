289ddbf9d98c53515afaaeef264403a5a6dfea60
Sandydht <sandydwi75@gmail.com>
2021-11-22 01:38:13 +0700

    Router configuration

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app" class="w-full max-w-screen-tablet mx-auto">
+    <app-bar></app-bar>
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AppBar from '@/components/AppBar.vue'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  components: {
+    AppBar
+  }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="homePage" class="mt-5">
+    <search-bar @handle-submit="submit"></search-bar>
+
+    <div class="p-5">
+      <movie-list-skeleton v-if="isLoading" :count="skeletonCount"></movie-list-skeleton>
+
+      <div v-else-if="!isLoading">
+        <alert-message v-if="searchResult.Response === 'False'" :message="searchResult.Error"></alert-message>
+        <movie-list v-else-if="searchResult.Response === 'True'"  :movies="searchResult"></movie-list>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SearchBar from '@/components/SearchBar.vue'
+import MovieList from '@/components/MovieList.vue'
+import AlertMessage from '@/components/AlertMessage.vue'
+import MovieListSkeleton from '@/components/MovieListSkeleton.vue'
+import MovieSource from '@/data/movie-source.js'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SearchBar,
+    MovieList,
+    AlertMessage,
+    MovieListSkeleton
+  },
+  data() {
+    return {
+      searchResult: {},
+      isLoading: null,
+      skeletonCount: 10
+    }
+  },
+  methods: {
+    submit(value) {
+      this.isLoading = true
+
+      MovieSource
+        .searchMovie(value)
+        .then(response => {
+          this.searchResult = response
+        })
+        .catch(err => {
+          console.log('error: ', err)
+        })
+        .finally(() => {
+          this.isLoading = false
+        })
+    }
   }
 }
 </script>
